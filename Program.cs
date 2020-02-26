a3da4f2aecd704ddabf3ad6c8c7c608fd1538b3d
Rige1234 <60926154+Rige1234@users.noreply.github.com>
2020-02-26 22:40:47 +0000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,11 +7,19 @@ namespace Voting
         static void Main(string[] args)
         {
             int totalCountries = 27;
-            int _yes = 23;
-            int _no = 3;
-            int abstain = 1;
             int population = 65;
             int memberStates = 55;
+
+            Console.WriteLine("Results should add up to 27 as that is the amount of countries participating");
+            Console.WriteLine("Enter amount of votes for yes");
+            int _yes = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter amount of votes for no");
+            int _no = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter amount of votes for yes");
+            int abstain = Convert.ToInt32(Console.ReadLine());
+
 
             int yes_pc = (int)(0.5f + (100f * _yes) / totalCountries);
 
@@ -22,11 +30,11 @@ namespace Voting
 
             if(yes_pc >= population)
             {
-                Console.WriteLine($"Final Result Approved as above minmum required, the result was {yes_pc}% of the votes to yes and {no_pc}% for no");
+                Console.WriteLine($"Final Result Approved as above minmum required, the result was {yes_pc}% of the votes to yes and {no_pc}% for no and {abstain_pc}% for votes on neither");
             }
             else
             {
-                Console.WriteLine($"Final Results Rejected as below minimum required, the result was {no_pc}% of the votes to yes and {yes_pc}% for no");
+                Console.WriteLine($"Final Results Rejected as below minimum required, the result was {no_pc}% of the votes to yes and {yes_pc}% for no and {abstain_pc}% for votes on neither");
             }
 
 
